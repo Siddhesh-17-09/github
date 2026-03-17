16efb37174806c19bcf4070f30c25165568f9860
Gangurde Siddhesh Sunil <2023ad20f@sigce.edu.in>
2026-03-17 14:12:34 +0530

    Update HelloServlet.java

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -1,1 +1,12 @@
+package com.example;
 
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+        res.getWriter().println("<h1>Hello from Jenkins Pipeline!</h1>");
+    }
+}
